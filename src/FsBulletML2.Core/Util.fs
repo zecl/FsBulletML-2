ef4e19af8162748138be03900f93679d57b8c5ff
zecl <416158+zecl@users.noreply.github.com>
2026-09-25 17:49:59 +0900

    refactor(core): 使われていない TryParseActivePattern と TryParse を消す
    - TryParseActivePattern は AutoOpen で、open FsBulletML2 した側 に Date / Int32 / Int64 / Single / Double / Decimal の active pattern を持ち込んでいた。同じ 名前 の腕 を持つ 型 を先 に open していると、その腕 で照合 できなく なる（FS0001）
    - 使っていた 所 は 0（本体・DanmakuLab・Docs、2013 年版 でも 0）。NuGet に出す 前 なので 外 に使う 人 は居ない
    - 使われていない open（System.Globalization / System.IO）も落とす
    - 試験 1 本: 名前空間 の外 で Single / Double / Date / Decimal / Int32 / Int64 を腕 に持つ 型 を先 に open し、open FsBulletML2 の あと で照合 する。消す 前 の Util.fs では build が FS0001 で落ちる

--- src/FsBulletML2.Core/Util.fs
+++ src/FsBulletML2.Core/Util.fs
@@ -1,35 +1,9 @@
 ﻿namespace FsBulletML2
 
 open System
-open System.Globalization
-open System.IO
 open System.Runtime.CompilerServices
 
 // BinaryFormatter は戻すな。深い複製が要る型が出たら、型の付いた clone を足す。
-
-[<RequireQualifiedAccess>]
-module internal TryParse =
-    let tryParseWith tryParseFunc =
-        tryParseFunc
-        >> function
-            | true, v -> Some v
-            | false, _ -> None
-
-    let parseDate = tryParseWith (fun (s: string) -> System.DateTime.TryParse(s))
-    let parseInt32 = tryParseWith (fun (s: string) -> System.Int32.TryParse(s))
-    let parseInt64 = tryParseWith (fun (s: string) -> System.Int64.TryParse(s))
-    let parseSingle = tryParseWith (fun (s: string) -> System.Single.TryParse(s))
-    let parseDouble = tryParseWith (fun (s: string) -> System.Double.TryParse(s))
-    let parseDecimal = tryParseWith (fun (s: string) -> System.Decimal.TryParse(s))
-
-[<AutoOpen>]
-module TryParseActivePattern =
-    let (|Date|_|) = TryParse.parseDate
-    let (|Int32|_|) = TryParse.parseInt32
-    let (|Int64|_|) = TryParse.parseInt64
-    let (|Single|_|) = TryParse.parseSingle
-    let (|Double|_|) = TryParse.parseDouble
-    let (|Decimal|_|) = TryParse.parseDecimal
 
 [<AutoOpen>]
 module Monad =
